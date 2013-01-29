315b1253d99f14a469bef2ea23ab3df667b41bdf
ThienL+HuyD <thien.lam+huy.dinh@eastagile.com>
2013-01-29 14:10:29 +0700

    init rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MaskedMessage::Application.config.secret_token = '8315d01302db24b0f67cb4ee48336d05ed6dcc25a9d124b0a6cb1344a482e26b4750d7f0b9284f37b24f2623c94948d7e2bfa74b2637fcc925110a71fce6513c'
